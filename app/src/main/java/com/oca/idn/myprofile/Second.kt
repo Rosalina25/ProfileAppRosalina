2302d1bdc2284aea94853510b34ff4a7dc715408
Rosalina25 <rosalina250102@gmail.com>
2020-09-18 17:38:33 +0700

    Upload Project

---
+++ app/src/main/java/com/oca/idn/myprofile/Second.kt
@@ -0,0 +1,11 @@
+package com.oca.idn.myprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Second : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
